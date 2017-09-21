3e974d57dbf1774adf230e78e18b43da15d0c0c0
tieshou <tieshou@baofu.com>
2017-09-21 18:20:31 +0800

    代码提交

--- lessontieshou.sql
+++ lessontieshou.sql
@@ -21,17 +21,18 @@ USE `lessontieshou`;
 DROP TABLE IF EXISTS `study`;
 
 CREATE TABLE `study` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT COMMENT '学生id号',
+  `id` bigint(11) unsigned NOT NULL AUTO_INCREMENT COMMENT '学生id号',
   `username` varchar(30) NOT NULL DEFAULT '' COMMENT '学生名字',
   `class` tinyint(3) unsigned NOT NULL,
   `sex` enum('男','女','保密') NOT NULL DEFAULT '保密' COMMENT '性别',
   `addtime` int(10) NOT NULL DEFAULT '0',
+  `password` varchar(20) NOT NULL COMMENT '密码',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COMMENT='学生表';
 
 /*Data for the table `study` */
 
-insert  into `study`(`id`,`username`,`class`,`sex`,`addtime`) values (1,'小王',1,'保密',0),(2,'小四',2,'女',0),(3,'小王',1,'男',5),(4,'小四',2,'女',7);
+insert  into `study`(`id`,`username`,`class`,`sex`,`addtime`,`password`) values (1,'小王',1,'保密',0,'123456'),(2,'小四',2,'女',0,'321654'),(3,'小王',1,'男',5,'12345'),(4,'小四',2,'女',7,'123213');
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
