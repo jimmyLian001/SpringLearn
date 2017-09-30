efeefc09cce73e111dbf8228ce77016c5eb552e6
tieshou <tieshou@baofu.com>
2017-09-30 13:20:05 +0800

    说明修改

--- src/main/java/com/jimmy/controller/FileProcessController.java
+++ src/main/java/com/jimmy/controller/FileProcessController.java
@@ -25,12 +25,12 @@ import java.util.Iterator;
 public class FileProcessController {
 
     @RequestMapping("upload.do")
-    public ModelAndView fileUpload(@RequestParam("uploadFile") CommonsMultipartFile[] files, HttpServletRequest request){
+    public ModelAndView fileUpload(@RequestParam("uploadFile") CommonsMultipartFile[] files, HttpServletRequest request) {
 
-        for(int i = 0;i<files.length;i++){
+        for (int i = 0; i < files.length; i++) {
             log.info("fileName---------->" + files[i].getOriginalFilename());
-
-            if(!files[i].isEmpty()){
+            //添加了.gitignore
+            if (!files[i].isEmpty()) {
                 int pre = (int) System.currentTimeMillis();
                 try {
                     //拿到输出流，同时重命名上传的文件
@@ -40,14 +40,14 @@ public class FileProcessController {
 
                     //以写字节的方式写文件
                     int b = 0;
-                    while((b=in.read()) != -1){
+                    while ((b = in.read()) != -1) {
                         os.write(b);
                     }
                     os.flush();
                     os.close();
                     in.close();
                     int finalTime = (int) System.currentTimeMillis();
-                    log.info("文件上传时间"+(finalTime - pre));
+                    log.info("文件上传时间" + (finalTime - pre));
 
                 } catch (Exception e) {
                     e.printStackTrace();
@@ -62,23 +62,23 @@ public class FileProcessController {
 
     /**
      * 通过流的方式上传文件
+     *
      * @RequestParam("file") 将name=file控件得到的文件封装成CommonsMultipartFile 对象
      */
     @RequestMapping("uploadStream.do")
-    public ModelAndView  fileUpload(@RequestParam("file") CommonsMultipartFile file) throws IOException {
+    public ModelAndView fileUpload(@RequestParam("file") CommonsMultipartFile file) throws IOException {
         //用来检测程序运行时间
-        long  startTime=System.currentTimeMillis();
-        log.info("fileName："+file.getOriginalFilename());
+        long startTime = System.currentTimeMillis();
+        log.info("fileName：" + file.getOriginalFilename());
 
         try {
             //获取输出流
-            OutputStream os=new FileOutputStream("E:/jimmyFile/"+new Date().getTime()+file.getOriginalFilename());
+            OutputStream os = new FileOutputStream("E:/jimmyFile/" + new Date().getTime() + file.getOriginalFilename());
             //获取输入流 CommonsMultipartFile 中可以直接得到文件的流
-            InputStream is=file.getInputStream();
+            InputStream is = file.getInputStream();
             int temp;
             //一个一个字节的读取并写入
-            while((temp=is.read())!=(-1))
-            {
+            while ((temp = is.read()) != (-1)) {
                 os.write(temp);
             }
             os.flush();
@@ -88,8 +88,8 @@ public class FileProcessController {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
-        long  endTime=System.currentTimeMillis();
-        log.info("方法一的运行时间："+String.valueOf(endTime-startTime)+"ms");
+        long endTime = System.currentTimeMillis();
+        log.info("方法一的运行时间：" + String.valueOf(endTime - startTime) + "ms");
         ModelAndView mv = new ModelAndView("redirect:/view/jsp/mcidInfoAdd.jsp");//默认为forward模式
         mv.addObject("message", "文件上传成功！");
         return mv;
@@ -99,16 +99,16 @@ public class FileProcessController {
      * 采用file.Transto 来保存上传的文件
      */
     @RequestMapping("uploadMultipart.do")
-    public ModelAndView  fileUpload2(@RequestParam("file") CommonsMultipartFile file) throws IOException {
-        long  startTime=System.currentTimeMillis();
-        log.info("fileName："+file.getOriginalFilename());
-        String path="E:/jimmyFile/"+new Date().getTime()+file.getOriginalFilename();
+    public ModelAndView fileUpload2(@RequestParam("file") CommonsMultipartFile file) throws IOException {
+        long startTime = System.currentTimeMillis();
+        log.info("fileName：" + file.getOriginalFilename());
+        String path = "E:/jimmyFile/" + new Date().getTime() + file.getOriginalFilename();
 
-        File newFile=new File(path);
+        File newFile = new File(path);
         //通过CommonsMultipartFile的方法直接写文件（注意这个时候）
         file.transferTo(newFile);
-        long  endTime=System.currentTimeMillis();
-        log.info("方法二的运行时间："+String.valueOf(endTime-startTime)+"ms");
+        long endTime = System.currentTimeMillis();
+        log.info("方法二的运行时间：" + String.valueOf(endTime - startTime) + "ms");
         ModelAndView mv = new ModelAndView("redirect:/view/jsp/mcidInfoAdd.jsp");//默认为forward模式
         mv.addObject("message", "文件上传成功！");
         return mv;
@@ -116,41 +116,37 @@ public class FileProcessController {
 
 
     /**
-     *采用spring提供的上传文件的方法
+     * 采用spring提供的上传文件的方法
      */
     @RequestMapping("uploadSpring.do")
-    public ModelAndView  springUpload(HttpServletRequest request) throws IllegalStateException, IOException
-    {
-        long  startTime=System.currentTimeMillis();
+    public ModelAndView springUpload(HttpServletRequest request) throws IllegalStateException, IOException {
+        long startTime = System.currentTimeMillis();
         //将当前上下文初始化给  CommonsMutipartResolver （多部分解析器）
-        CommonsMultipartResolver multipartResolver=new CommonsMultipartResolver(
+        CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver(
                 request.getSession().getServletContext());
         //检查form中是否有enctype="multipart/form-data"
-        if(multipartResolver.isMultipart(request))
-        {
+        if (multipartResolver.isMultipart(request)) {
             //将request变成多部分request
-            MultipartHttpServletRequest multiRequest=(MultipartHttpServletRequest)request;
+            MultipartHttpServletRequest multiRequest = (MultipartHttpServletRequest) request;
             //获取multiRequest 中所有的文件名
-            Iterator iter=multiRequest.getFileNames();
+            Iterator iter = multiRequest.getFileNames();
 
-            while(iter.hasNext())
-            {
+            while (iter.hasNext()) {
                 //一次遍历所有文件
-                MultipartFile file=multiRequest.getFile(iter.next().toString());
-                if(file!=null)
-                {
-                    String path="E:/jimmyFile/springUpload/"+file.getOriginalFilename();
+                MultipartFile file = multiRequest.getFile(iter.next().toString());
+                if (file != null) {
+                    String path = "E:/jimmyFile/springUpload/" + file.getOriginalFilename();
                     //上传
                     file.transferTo(new File(path));
                 }
             }
         }
-        long  endTime=System.currentTimeMillis();
-        log.info("方法三的运行时间："+String.valueOf(endTime-startTime)+"ms");
-        ModelAndView modelAndView=new ModelAndView("forward:/view/jsp/mcidInfoAdd.jsp");
+        long endTime = System.currentTimeMillis();
+        log.info("方法三的运行时间：" + String.valueOf(endTime - startTime) + "ms");
+        ModelAndView modelAndView = new ModelAndView("forward:/view/jsp/mcidInfoAdd.jsp");
         modelAndView.addObject("message", "文件上传成功！");
         return modelAndView;
     }
 
 
-    }
+}
