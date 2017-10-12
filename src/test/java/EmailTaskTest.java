c69a7cf054e0a6aed86367f9c593abcc8ad91471
tieshou <tieshou@baofu.com>
2017-10-12 13:50:20 +0800

    添加测试类

---
+++ src/test/java/EmailTaskTest.java
@@ -0,0 +1,27 @@
+import com.jimmy.job.EmailTask;
+import junit.framework.TestCase;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.slf4j.MDC;
+import org.slf4j.helpers.SystemMarker;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.UUID;
+
+/**
+ * <p>
+ * User: lian zd Date:2017/10/12 ProjectName: spring_learn1 Version: 1.0
+ */
+@Slf4j
+public class EmailTaskTest extends TestCase {
+
+    @Autowired
+    private EmailTask emailTask;
+
+    @Test
+    public void testEmailTask(){
+        MDC.put(SystemMarker.TRACE_LOG_ID, UUID.randomUUID().toString());
+        log.info("现在开始测试Junit");
+//        emailTask.sendEmailByScheduled();
+    }
+}
