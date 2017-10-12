5ee429fc207013aaa60e6f67964c9ca395ea3a2a
tieshou <tieshou@baofu.com>
2017-10-12 16:16:35 +0800

    添加Junit测试依赖Bean文件

--- src/test/java/EmailTaskTest.java
+++ src/test/java/EmailTaskTest.java
@@ -10,18 +10,19 @@ import java.util.UUID;
 
 /**
  * <p>
+ * extends  TestCase
  * User: lian zd Date:2017/10/12 ProjectName: spring_learn1 Version: 1.0
  */
 @Slf4j
-public class EmailTaskTest extends TestCase {
+public class EmailTaskTest extends BaseTest {
 
     @Autowired
     private EmailTask emailTask;
 
     @Test
-    public void testEmailTask(){
+    public void emailTaskTest(){
         MDC.put(SystemMarker.TRACE_LOG_ID, UUID.randomUUID().toString());
         log.info("现在开始测试Junit");
-//        emailTask.sendEmailByScheduled();
+        emailTask.sendEmailByScheduled();
     }
 }
